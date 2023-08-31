7ffa6c3127b2c1fb5fe47a43e2984afb8eab222d
Diana Pulatova <dianapulatovaa@gmail.com>
2023-08-31 13:47:11 -0400

    added php file and linked the email address

--- assets/contact.php
+++ assets/contact.php
@@ -1,19 +1,23 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $first_name = $_POST["first_name"];
+  $last_name = $_POST["last_name"];
+  $email = $_POST["email"];
+  $message = $_POST["text"];
 
-// Get the form data
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+  $to = "dianapulatovaa@email.com"; // Replace with your email address
+  $subject = "New Contact Form Submission";
+  $headers = "From: $email";
 
-// Send an email
-mail('dianapulatovaa@gmail.com', 'New Contact Form Submission',
-    'First Name: ' . $first_name . "\n" .
-    'Last Name: ' . $last_name . "\n" .
-    'Email: ' . $email . "\n" .
-    'Message: ' . $message);
+  $message_body = "Name: $first_name $last_name\n";
+  $message_body .= "Email: $email\n";
+  $message_body .= "Message:\n$message";
 
-// Redirect to the thank you page
-header('Location: thank-you.php');
+  mail($to, $subject, $message_body, $headers);
 
+  // You can also redirect to a thank you page or display a success message
+  header("Location: thank-you.html");
+  exit;
+}
 ?>
+
